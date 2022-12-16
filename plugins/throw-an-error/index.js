338cab6f1be7950e81516020736ba1c165407164
Suzanne Aitchison <suzanne.aitchison@netlify.com>
2022-12-16 11:58:19 +0000

    try with diff error message

--- plugins/throw-an-error/index.js
+++ plugins/throw-an-error/index.js
@@ -1,6 +1,8 @@
 module.exports = {
   onPreBuild: () => {
     console.log('About to throw a random error - wooo');
-    throw new Error('something went terribly wrong!!!');
+    throw new Error(
+      'Command failed with exit code: something went terribly wrong!!!'
+    );
   },
 };
