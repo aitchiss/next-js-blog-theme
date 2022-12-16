647ef018e97f7a9992d4d81df101130c9e7aa186
Suzanne Aitchison <suzanne.aitchison@netlify.com>
2022-12-16 12:16:04 +0000

    alphanumeric

--- plugins/throw-an-error/index.js
+++ plugins/throw-an-error/index.js
@@ -2,7 +2,7 @@ module.exports = {
   onPreBuild: ({ utils }) => {
     console.log('About to throw a random error - wooo');
     try {
-      throw new Error('Command failed with exit code: 67565');
+      throw new Error('Command failed with exit code: 6MJ7565');
     } catch (error) {
       utils.build.failPlugin('MY ERROR MESSAGE', { error });
     }
