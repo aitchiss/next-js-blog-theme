4365006b6edba1364921b0a257eaa0242403731c
Suzanne Aitchison <suzanne.aitchison@netlify.com>
2022-12-16 12:24:15 +0000

    try with command failed in the function call

--- plugins/throw-an-error/index.js
+++ plugins/throw-an-error/index.js
@@ -2,9 +2,11 @@ module.exports = {
   onPreBuild: ({ utils }) => {
     console.log('About to throw a random error - wooo');
     try {
-      throw new Error('Command failed with exit code: 6MJ7565');
+      throw new Error('something');
     } catch (error) {
-      utils.build.failPlugin('MY ERROR MESSAGE', { error });
+      utils.build.failPlugin('Command failed with exit code: 6MJ7565', {
+        error,
+      });
     }
   },
 };
