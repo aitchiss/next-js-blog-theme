12c31c66540aa3a21e6e4b504723c3b9f7c6f946
Suzanne Aitchison <suzanne.aitchison@netlify.com>
2022-12-21 16:41:06 +0000

    onend plugin

--- plugins/throw-an-error/index.js
+++ plugins/throw-an-error/index.js
@@ -1,12 +1,5 @@
 module.exports = {
-  onPreBuild: ({ utils }) => {
-    console.log('About to throw a random error - wooo');
-    try {
-      throw new Error('something');
-    } catch (error) {
-      utils.build.failPlugin('Command failed with exit code: 6MJ7565', {
-        error,
-      });
-    }
+  onEnd: () => {
+    console.log('PLUGIN RUNNING ON END WOOOOOOOOO!');
   },
 };
